60df40e32c816c347574fb34ae54fd5c3bfdfeff
David Bauman <david.bauman@oxfordecosystems.co.uk>
2023-07-17 08:32:41 +0200

    Added command lines to R script to remember how to push existing repo. (on git) to github

--- Script_sandbox_rstudio_github.R
+++ Script_sandbox_rstudio_github.R
@@ -1,2 +1,6 @@
 # This is a test
 
+# In Terminal: To push an existing repo. from command line, after creating repo. on Github:
+# git remote add origin https://github.com/davbauman/test.git
+# git branch -M main
+# git push -u origin main
